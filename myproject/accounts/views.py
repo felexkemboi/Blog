7151e08357accaf6dcf85ca92c74c9751de23ddb
felexkemboi <felokemboi10@gmail.com>
2018-03-23 17:33:14 +0300

    Remove UserCreationForm and use SignUpForm

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render,render,redirect
+from django.shortcuts import render,redirect
 from django.contrib.auth import login as auth_login
 
+from .forms import SignUpForm
 
 
+"""
 def signup(request):
 	if request.method =='POST':
 		form = UserCreationForm(request.POST)
@@ -12,4 +14,14 @@ def signup(request):
 			return redirect('home')
 	else:
 		form =  UserCreationForm()
-	return render(request, 'signup.html', {'form': form})
+	return render(request, 'signup.html', {'form': form})"""
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
