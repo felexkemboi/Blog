65e795ef2cde7c765efe9c5dcdf5112a8ca1e375
felexkemboi <felokemboi10@gmail.com>
2018-03-23 13:34:27 +0300

    first commit

---
+++ myproject/boards/forms.py
@@ -0,0 +1,10 @@
+from .models import Topic
+from django import forms
+from .models import Topic
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows':5,'placeholder':'what is on your mind?'}), max_length=4000,help_text = 'Maximum length of the text is 4000')
+
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
---
+++ myproject/myproject/urls.py
@@ -0,0 +1,15 @@
+
+from django.conf.urls import url
+from django.contrib import admin
+
+from boards import views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    url(r'^$', views.home,name='home'),
+    url(r'^signup/$', accounts_views.signup,name='signup'),
+    url(r'^boards/(?P<pk>\d+)/$',views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
+
+]
