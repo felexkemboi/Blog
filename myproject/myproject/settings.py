835f1d16304a4c83fd88b0ff4c4afd25a5d3d061
felexkemboi <felokemboi10@gmail.com>
2018-03-23 22:27:53 +0300

    redirect the user to the homepage whenever the user logout

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+LOGOUT_REDIRECT_URL = 'home'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static'),
