b9e56b49d545fe7acd5108c92ab7407494854f98
felexkemboi <felokemboi10@gmail.com>
2018-03-28 13:47:15 +0300

    add reply form to the forms file

--- myproject/boards/forms.py
+++ myproject/boards/forms.py
@@ -8,3 +8,8 @@ class NewTopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['subject', 'message']
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model =Post
+		fields = ['message',]
